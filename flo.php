9ebcf7bde2dd7c739541bad7cffde09fbeaf95b8
JasonLeeH <jason@anon.so>
2014-02-26 00:01:43 -0500

    Character Differentiation
    Added support for 4 character differentiation

--- flo.php
+++ flo.php
@@ -7,11 +7,21 @@
 	while ($i<=$wordcount) {
 		$word = $srcRef[$i];
 		$wlen = strlen($word);
+		if ($wlen = 4) {
+			$wlen = strlen($word);
+			$lPad = floor($wlen/2)+1;
+			$rPad = ceil($wlen/2);
+			$left = substr($word, 0, -$lPad);
+			$middle = substr($word, -$lPad, 1); 
+			$right = substr($word, $rPad, $lPad);
+			$floWord = "   ".$left.ColorCLI::light_red($middle, null, 'null').$right;
+		} else {
 		$lPad = floor($wlen/2);
 		$left = substr($word, 0, $lPad);
 		$middle = substr($word, $lPad, 1);
 		$right = substr($word, $lPad + 1);
 		$floWord = "  ".$left.ColorCLI::light_red($middle, null, 'null').$right;
+		}
 		echo str_pad($floWord, 34, " ", STR_PAD_BOTH)."\r";
 		$i++;
 		usleep(120000);
