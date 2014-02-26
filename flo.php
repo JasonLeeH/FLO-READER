df4ff534fd863cffeb56dd3d2ead2fbdda78b661
JasonLeeH <jason@anon.so>
2014-02-25 23:12:33 -0500

    Formatting Changes
    Added in-line CLI color for better readability, as well as an extended
    padding on either side of the words.

--- flo.php
+++ flo.php
@@ -1,4 +1,5 @@
 <?php
+	include('cli_color.php');
 	$src = file_get_contents('Alice.txt');
 	$srcRef = preg_split("/[\s]+/", $src);
 	$wordcount = count($srcRef);
@@ -10,8 +11,8 @@
 		$left = substr($word, 0, $lPad);
 		$middle = substr($word, $lPad, 1);
 		$right = substr($word, $lPad + 1);
-		$floWord = $left . '<font color="#B40431">'.$middle.'</font>'.$right."\r";
-		echo str_pad($floWord, 24, " ", STR_PAD_BOTH)."\r";
+		$floWord = "  ".$left.ColorCLI::light_red($middle, null, 'null').$right;
+		echo str_pad($floWord, 34, " ", STR_PAD_BOTH)."\r";
 		$i++;
 		usleep(200000);
 	}
